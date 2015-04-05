81aad65e9ccb15653891b19c86382b10e6bdcaed
tart2000 <tart2000design@gmail.com>
2015-04-04 23:44:51 -0400

    footer better

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -3,7 +3,10 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4">
-
+                <strong>Projets</strong></br>
+                <?php foreach (page('projects')->children() as $project): ?>
+                    <a href="<?php $project->projectLink() ?>"><?php echo $project->title() ?></a></br>
+                <?php endforeach ?>
             </div>
             <div class="col-md-2 col-md-offset-1">
                 <a href="http://technoculture.club" target="_blank"><img src="/assets/images/logo_TCC_M.png" class="img-responsive"></a>
@@ -13,8 +16,11 @@
                     <a href="<?php $site->linkedin() ?>"><i class="fa fa-linkedin fa-2x"></i></a>
                 </div>
             </div>
-            <div class="col-md-4 col-md-offset-1">
-
+            <div class="col-md-4 col-md-offset-1 right">
+                <strong>Contact</strong>
+                <p>Techno Culture Club<br> 
+                7255 Alexandra #203<br> 
+                Montréal, QC</p>
             </div>
         </div>
     </div>
