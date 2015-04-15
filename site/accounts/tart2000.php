b8d5a4014cf50833c5f24c4dfeaa2672fd358f53
tart2000 <tart2000design@gmail.com>
2015-04-15 17:58:44 -0400

    page événements : en avant !

--- site/accounts/tart2000.php
+++ site/accounts/tart2000.php
@@ -8,8 +8,8 @@ language: fr
 role: admin
 token: 1de31bf873de7c5537bcc7394af594303695bd2b
 history:
+  - evenements/space-apps-challenge-2015
   - evenements/museomix-2014
   - evenements/powop-summer-1
-  - evenements/space-apps-challenge-2015
   - evenements/lancement-de-museomix-2015
   - evenements/chromatic-kids
---
+++ site/templates/evenements.php
@@ -0,0 +1,72 @@
+<?php snippet('head') ?>
+<?php snippet('header') ?>
+
+  <main class="container bmt">
+
+    <div class="row center">
+      <h2><?php echo $page->title()->html() ?></h2>
+      <?php echo $page->text()->kirbytext() ?>
+    </div>
+
+  <?php foreach(page()->children()->sortBy('startDate') as $event): ?>
+    <?php $thedate=strtotime(date("Y-m-d"))?>
+    <?php $thisdate = $event->date('','startDate') ?>
+    <?php if ($thedate <= $thisdate) : ?>
+    <div class="row">
+      <div class="col-md-10 col-md-offset-1">
+          <!-- si c'est sur plusieurs jours -->
+          <?php if ($event->endDate() != '') : ?>
+            <h3><i class="fa fa-calendar"></i> <?php echo $event->date('j','startDate') ?>-<?php echo $event->date('j M','endDate') ?> - <a href="<?php echo $event->url() ?>"><?php echo $event->title()->html() ?></a></h3>
+            <?php if ($event->venueLink() != '') : ?>
+              <a href="<?php echo $event->venueLink() ?>" target="_blank"><strong><?php echo $event->venueName() ?></strong></a>
+            <?php else : ?>
+              <strong><?php echo $event->venueName() ?></strong>
+            <?php endif ?>
+          <?php else : ?>
+          <!-- si c'est sur un seul jour -->
+            <h3><i class="fa fa-calendar"></i> <?php echo $event->date('j M','startDate') ?> - <a href="<?php echo $event->url() ?>"><?php echo $event->title()->html() ?></a></h3>
+            <strong><?php echo $event->startTime() ?> - <?php echo $event->endTime() ?> <?php echo $event->venueName() ?></strong>
+          <?php endif ?>
+        
+        <?php if ($event->registrationLink() != '') : ?>
+          <div class="register">
+            <a href="<?php echo $event->registrationLink() ?>" class="btn btn-theme" target="_blank">Inscriptions</a>
+          </div>
+        <?php endif ?>
+        
+        <p><?php echo $event->text() ?></p>
+      </div>
+    </div>
+    <?php endif ?>
+  <?php endforeach ?>
+
+  <hr>
+  <div class="row center bmt">
+    <h3>Evénements passés</h3> 
+  </div>
+    <?php foreach(page()->children()->sortBy('startDate') as $event): ?>
+    <?php $thedate=strtotime(date("Y-m-d"))?>
+    <?php $thisdate = $event->date('','startDate') ?>
+    <?php if ($thedate > $thisdate) : ?>
+    <div class="row mt">
+      <div class="col-md-10 col-md-offset-1">
+          <!-- si c'est sur plusieurs jours -->
+          <?php if ($event->endDate() != '') : ?>
+            <strong><?php echo $event->date('j','startDate') ?>-<?php echo $event->date('j M','endDate') ?> - <?php echo $event->title()->html() ?> - <?php echo $event->venueName() ?></strong>
+          <?php else : ?>
+          <!-- si c'est sur un seul jour -->
+             <strong><?php echo $event->date('j M','startDate') ?> - <?php echo $event->title()->html() ?> - <?php echo $event->venueName() ?></strong>
+          <?php endif ?>
+        
+        <p><?php echo $event->text() ?></p>
+      </div>
+    </div>
+    <?php endif ?>
+  <?php endforeach ?>
+
+
+
+
+  </main>
+
+<?php snippet('footer') ?>
