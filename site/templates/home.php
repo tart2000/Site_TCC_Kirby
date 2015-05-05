c9ba6dbf73a5bd755b178e577adff0109d914f20
tart2000 <tart2000design@gmail.com>
2015-05-04 23:25:00 -0400

    changes to homepage

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,36 +1,36 @@
 <?php snippet('head') ?>
 <?php snippet('header') ?>
 
-<div id="nymphe">
+<div id="intro">
+    <main class="container bmt">
+        <div class="row bmt">
+            <div class="col-md-3 col-sm-4  col-sm-offset-1">
+                <img src="/assets/images/logo_TCC_M.png" class="img-responsive">
+            </div>
+            <div class="col-md-6 col-md-offset-1 col-sm-6 left bmt white-bg">
+                <h3><?php echo $page->text()->kirbytext() ?></h3>
+                <a href="/a-propos/" class="btn btn-lg btn-theme">En savoir plus...</a>
+            </div>
+        </div><!-- /row -->
+    </main>
+</div>
+
+
+
+<?php snippet('sticky2') ?>
+
+
+<?php snippet('evenements') ?>
+
+<div id="nymphe" class="bmt">
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1 center">
-                <h1>Bienvenue au Techno Culture Club !</h1>
-                <h1>Au croisement de la culture et de la technologie.</h1>
+                <h1>Technologie X Culture</h1>
             </div>
         </div>
     </div>
 </div>
-
-<?php snippet('sticky2') ?>
-
-<main class="container">
-
-    <div class="row bmt">
-        <div class="col-md-3 col-sd-6 col-xs-6 col-md-offset-1">
-            <img src="/assets/images/logo_TCC_M.png" class="img-responsive">
-        </div>
-        <div class="col-md-6 col-md-offset-1 left bmt">
-            <h3><?php echo $page->text()->kirbytext() ?></h3>
-            <a href="/a-propos/" class="btn btn-lg btn-theme">En savoir plus...</a>
-        </div>
-    </div><!-- /row -->
-
-</main>
-
-<hr class="bmt">
-
-<?php snippet('evenements') ?>
 
 <div id="projects" class="bmt pt pb">
     <div class="container">
