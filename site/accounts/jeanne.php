5378b034043088ac543c22400ab96e9a898c1879
Arvakb <jibertoux@gmail.com>
2015-10-17 13:46:21 -0400

    new account

---
+++ site/accounts/jeanne.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: jeanne
+firstname: Jeanne
+lastname: Bertoux
+email: jibertoux@gmail.com
+password: >
+  $2a$10$GgLNBQc.ql4r0yD8FxXod.I2MdrkMxiC9BLgSMp6er2IqT1lv8nue
+language: fr
+role: admin
+token: 12fc04032b14b751f94a8aef1efcff400e245dd3
+history:
+  - projets/Museomix/museomix-2015
+  - calls/fiche-de-poste-attache-de-presse
