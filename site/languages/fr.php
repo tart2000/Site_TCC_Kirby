48334b9b9682da2a18ab7a2bc723f0d9e41881b4
Ariel Harlap <aharlap@gmail.com>
2016-08-10 12:34:19 -0400

    Added cookie policy

--- site/languages/fr.php
+++ site/languages/fr.php
@@ -14,5 +14,8 @@ l::set('coming-edition','Édition à venir');
 l::set('news', 'nouvelles');
 l::set('join-team','Rejoignez notre équipe !');
 l::set('see','Voir');
+l::set('cookie-message','Ce site utilise des cookies pour assurer que vous obtenez la meilleure expérience sur notre site');
+l::set('cookie-dismiss',"D'accord!");
+l::set('cookie-learn-more',"Plus d'infos");
 
 ?>
--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -35,6 +35,13 @@
   <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
   <script src="<?php echo $site->url() ?>/assets/js/jquery.countdown.min.js"></script>
   <script src="<?php echo $site->url() ?>/assets/js/main.js"></script>
+<!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+<script type="text/javascript">
+    window.cookieconsent_options = {"message":<?php echo l::get('cookie-message') ?>,"dismiss":<?php echo l::get('cookie-dismiss') ?>,"learnMore":<?php echo l::get('cookie-learn-more') ?>,"link":"/en/cookie","theme":"dark-top"};
+</script>
+
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
+<!-- End Cookie Consent plugin -->
 
 </body>
 </html>
