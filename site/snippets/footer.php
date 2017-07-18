e2da1d1c4625c33cdfe09da67d746d22e7d3f09c
Ariel Harlap <aharlap@gmail.com>
2017-07-17 21:53:49 -0400

    Rebranding sections

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -3,17 +3,11 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-4">
-                <strong><?php echo page('services')->title() ?></strong></br>
-                <?php foreach (page('services')->children()->visible() as $service): ?>
-                    <a href="<?php echo $service->url() ?>"><?php echo $service->title() ?></a></br>
-                <?php endforeach ?>
-
-                </br>
-                <strong><?php echo page('projets')->title() ?></strong></br>
-                <?php foreach (page('projets')->children()->visible() as $service): ?>
-                    <a href="<?php echo $service->url() ?>"><?php echo $service->title() ?></a></br>
-                <?php endforeach ?>
-
+              <p>
+              <?php foreach($pages->visible() as $p): ?>
+              <a href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a></br>
+              <?php endforeach ?>
+              </p>
             </div>
 
 
@@ -26,7 +20,7 @@
                 </div>
             </div>
             <div class="col-sm-4 col-sm-offset-1 right">
-                <strong>Contact</strong>
+                <strong><a href="contact">Contact</a></strong>
                 <p>Techno Culture Club<br>
                 2275 boul Saint-Joseph E #1.120<br>
                 Montréal, QC</p>
